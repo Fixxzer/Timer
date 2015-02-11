e8acf53282a1248c0aad0ebbc03b9536c4529c2d
Fixxzer <jakegarner@hotmail.com>
2015-02-11 16:40:46 -0700

    Initial commit.

---
+++ Timer/Form1.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace Timer
+{
+    partial class FormTimer
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FormTimer));
+            this.lblDate = new System.Windows.Forms.Label();
+            this.monthCalendar1 = new System.Windows.Forms.MonthCalendar();
+            this.lblTime = new System.Windows.Forms.Label();
+            this.lblStopwatchOutput = new System.Windows.Forms.Label();
+            this.btnStartStop = new System.Windows.Forms.Button();
+            this.btnReset = new System.Windows.Forms.Button();
+            this.notifyIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.contextMenuStrip = new System.Windows.Forms.ContextMenuStrip(this.components);
+            this.exitToolStripMenuItem = new System.Windows.Forms.ToolStripMenuItem();
+            this.timerCurrentDateTime = new System.Windows.Forms.Timer(this.components);
+            this.timerStopwatch = new System.Windows.Forms.Timer(this.components);
+            this.contextMenuStrip.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // lblDate
+            // 
+            this.lblDate.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblDate.ForeColor = System.Drawing.Color.MediumBlue;
+            this.lblDate.Location = new System.Drawing.Point(13, 13);
+            this.lblDate.Name = "lblDate";
+            this.lblDate.Size = new System.Drawing.Size(397, 23);
+            this.lblDate.TabIndex = 0;
+            this.lblDate.Text = "label1";
+            this.lblDate.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // monthCalendar1
+            // 
+            this.monthCalendar1.Location = new System.Drawing.Point(18, 45);
+            this.monthCalendar1.Name = "monthCalendar1";
+            this.monthCalendar1.TabIndex = 1;
+            // 
+            // lblTime
+            // 
+            this.lblTime.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblTime.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTime.Location = new System.Drawing.Point(258, 45);
+            this.lblTime.Name = "lblTime";
+            this.lblTime.Size = new System.Drawing.Size(152, 40);
+            this.lblTime.TabIndex = 2;
+            this.lblTime.Text = "label1";
+            this.lblTime.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblStopwatchOutput
+            // 
+            this.lblStopwatchOutput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.lblStopwatchOutput.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblStopwatchOutput.Location = new System.Drawing.Point(258, 107);
+            this.lblStopwatchOutput.Name = "lblStopwatchOutput";
+            this.lblStopwatchOutput.Size = new System.Drawing.Size(152, 45);
+            this.lblStopwatchOutput.TabIndex = 3;
+            this.lblStopwatchOutput.Text = "00:00:00.00";
+            this.lblStopwatchOutput.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // btnStartStop
+            // 
+            this.btnStartStop.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnStartStop.Location = new System.Drawing.Point(257, 155);
+            this.btnStartStop.Name = "btnStartStop";
+            this.btnStartStop.Size = new System.Drawing.Size(153, 23);
+            this.btnStartStop.TabIndex = 4;
+            this.btnStartStop.Text = "Start";
+            this.btnStartStop.UseVisualStyleBackColor = true;
+            this.btnStartStop.Click += new System.EventHandler(this.BtnStartStop_Click);
+            // 
+            // btnReset
+            // 
+            this.btnReset.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.btnReset.Location = new System.Drawing.Point(257, 184);
+            this.btnReset.Name = "btnReset";
+            this.btnReset.Size = new System.Drawing.Size(153, 23);
+            this.btnReset.TabIndex = 5;
+            this.btnReset.Text = "Reset";
+            this.btnReset.UseVisualStyleBackColor = true;
+            this.btnReset.Click += new System.EventHandler(this.BtnReset_Click);
+            // 
+            // notifyIcon
+            // 
+            this.notifyIcon.ContextMenuStrip = this.contextMenuStrip;
+            this.notifyIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("notifyIcon.Icon")));
+            this.notifyIcon.Text = "Clock Timer";
+            this.notifyIcon.Visible = true;
+            this.notifyIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.NotifyIcon_MouseClick);
+            // 
+            // contextMenuStrip
+            // 
+            this.contextMenuStrip.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.exitToolStripMenuItem});
+            this.contextMenuStrip.Name = "contextMenuStrip";
+            this.contextMenuStrip.Size = new System.Drawing.Size(93, 26);
+            // 
+            // exitToolStripMenuItem
+            // 
+            this.exitToolStripMenuItem.Name = "exitToolStripMenuItem";
+            this.exitToolStripMenuItem.Size = new System.Drawing.Size(92, 22);
+            this.exitToolStripMenuItem.Text = "E&xit";
+            this.exitToolStripMenuItem.Click += new System.EventHandler(this.ExitToolStripMenuItem_Click);
+            // 
+            // timerCurrentDateTime
+            // 
+            this.timerCurrentDateTime.Enabled = true;
+            this.timerCurrentDateTime.Tick += new System.EventHandler(this.TimerCurrentDateTime_Tick);
+            // 
+            // timerStopwatch
+            // 
+            this.timerStopwatch.Tick += new System.EventHandler(this.TimerStopwatch_Tick);
+            // 
+            // FormTimer
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.ClientSize = new System.Drawing.Size(422, 226);
+            this.Controls.Add(this.btnReset);
+            this.Controls.Add(this.btnStartStop);
+            this.Controls.Add(this.lblStopwatchOutput);
+            this.Controls.Add(this.lblTime);
+            this.Controls.Add(this.monthCalendar1);
+            this.Controls.Add(this.lblDate);
+            this.DoubleBuffered = true;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.MaximumSize = new System.Drawing.Size(438, 265);
+            this.MinimumSize = new System.Drawing.Size(438, 265);
+            this.Name = "FormTimer";
+            this.ShowInTaskbar = false;
+            this.Text = "Clock Timer";
+            this.WindowState = System.Windows.Forms.FormWindowState.Minimized;
+            this.Resize += new System.EventHandler(this.FormTimer_Resize);
+            this.contextMenuStrip.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblDate;
+        private System.Windows.Forms.MonthCalendar monthCalendar1;
+        private System.Windows.Forms.Label lblTime;
+        private System.Windows.Forms.Label lblStopwatchOutput;
+        private System.Windows.Forms.Button btnStartStop;
+        private System.Windows.Forms.Button btnReset;
+        private System.Windows.Forms.NotifyIcon notifyIcon;
+        private System.Windows.Forms.ContextMenuStrip contextMenuStrip;
+        private System.Windows.Forms.ToolStripMenuItem exitToolStripMenuItem;
+        private System.Windows.Forms.Timer timerCurrentDateTime;
+        private System.Windows.Forms.Timer timerStopwatch;
+    }
+}
+
---
+++ Timer/Form1.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Timer
+{
+    public partial class FormTimer : Form
+    {
+        public FormTimer()
+        {
+            InitializeComponent();
+        }
+
+        private void ExitToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+
+        private void FormTimer_Resize(object sender, EventArgs e)
+        {
+            if (WindowState == FormWindowState.Minimized)
+            {
+                ShowInTaskbar = false;
+            }
+        }
+
+        private void NotifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            WindowState = FormWindowState.Normal;
+            ShowInTaskbar = true;
+        }
+
+        private void TimerCurrentDateTime_Tick(object sender, EventArgs e)
+        {
+            var dt = DateTime.Now;
+            lblDate.Text = dt.ToString("dddd, MMMM dd, yyyy");
+            lblTime.Text = dt.ToString("h:mm:ss tt");
+        }
+
+        #region Stopwatch
+
+        private static bool _isStopwatchRunning;
+        private readonly Stopwatch _stopwatch = new Stopwatch();
+
+        private void BtnStartStop_Click(object sender, EventArgs e)
+        {
+            if (_isStopwatchRunning)
+            {
+                btnStartStop.Text = "Start";
+                _stopwatch.Stop();
+            }
+            else
+            {
+                btnStartStop.Text = "Stop";
+                _stopwatch.Start();
+            }
+
+            _isStopwatchRunning = !_isStopwatchRunning;
+            timerStopwatch.Enabled = _isStopwatchRunning;
+        }
+
+        private void BtnReset_Click(object sender, EventArgs e)
+        {
+            _stopwatch.Reset();
+            lblStopwatchOutput.Text = string.Format("{0:hh\\:mm\\:ss\\.ff}", _stopwatch.Elapsed);
+
+            if (_isStopwatchRunning)
+            {
+                _stopwatch.Start();
+            }
+        }
+
+        private void TimerStopwatch_Tick(object sender, EventArgs e)
+        {
+            lblStopwatchOutput.Text = string.Format("{0:hh\\:mm\\:ss\\.ff}", _stopwatch.Elapsed);
+        }
+
+        #endregion
+    }
+}
